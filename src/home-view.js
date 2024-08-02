e12cbc507fd0da3f692db558b130bb6b0381349a
Samuel Davies <samj.davies@virginmedia.com>
2024-08-02 16:18:43 +0100

    Completed more of todolist

--- src/home-view.js
+++ src/home-view.js
@@ -1,11 +1,13 @@
 import './style.css';
 
 import todolistIcon from "./images/header/to-do-list.png";
+import Todo from "./todo.js";
 
 function createHomeView() {
     const main = document.getElementById("main");
     main.textContent = "";
     main.appendChild(createHeader());
+    main.appendChild(createMainContent());
 }
 
 function createHeader() {
@@ -17,6 +19,68 @@ function createHeader() {
     header.appendChild(headerImage);
     header.appendChild(headerTitle);
     return header;
+}
+
+function createMainContent() {
+    const mainContent = document.createElement("div");
+    mainContent.classList.add("main-content");
+
+    mainContent.appendChild(createSideBar());
+    let testTodo = new Todo("Welcome to What-To-Do", "A very simple todo list that only has what matters", "12/04/2024", "Low", false);
+
+    mainContent.appendChild(createTodo(testTodo));
+    return mainContent;
+}
+
+function createSideBar() {
+    const sideBar = document.createElement("div");
+    sideBar.classList.add("sideBar");
+
+    const projects = document.createElement("div");
+    projects.classList.add("projects");
+
+    const projectsTitle = document.createElement("h1");
+    projectsTitle.textContent = "My Projects";
+
+    const projectsList = document.createElement("ul");
+    projectsList.classList.add("projects-list");
+
+    const homeProject = document.createElement("li");
+    homeProject.textContent = "Home";
+
+    sideBar.appendChild(projectsTitle);
+    sideBar.appendChild(projects);
+    projects.appendChild(projectsList);
+    projectsList.appendChild(homeProject);
+    return sideBar;
+}
+
+function createTodo(newTodo) {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoHeading = document.createElement("h1");
+    todoHeading.textContent = newTodo.title;
+
+    const todoDesc = document.createElement("p");
+    todoDesc.textContent = newTodo.description;
+
+    const todoDate = document.createElement("p");
+    todoDate.textContent = newTodo.dueDate;
+
+    const priority = document.createElement("p");
+    priority.textContent = newTodo.priority;
+
+    const isCompleted = document.createElement("p");
+    isCompleted.textContent = newTodo.isCompleted;
+
+    todo.appendChild(todoHeading);
+    todo.appendChild(todoDesc);
+    todo.appendChild(todoDate);
+    todo.appendChild(priority);
+    todo.appendChild(isCompleted);
+
+    return todo;
 }
 
 export default createHomeView;
---
+++ src/todo.js
@@ -0,0 +1,40 @@
+class Todo {
+    
+    constructor(title, description, dueDate, priority, isCompleted) {
+        this._title = title;
+        this._description = description;
+        this._dueDate = dueDate;
+        this._priority = priority;
+        this._isCompleted = isCompleted;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    get isCompleted() {
+        return this._isCompleted;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(newTitle) {
+        this._title = newTitle;
+    }
+}
+
+export default Todo;
